7b33ce973443f06ee3782560bea66377240cd89d
ChiomaE <chioma.eriken@gmail.com>
2024-03-14 13:15:15 -0500

    access artists and songs by id

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
 const express = require("express")
 const app = express();
-const port = 3000;
+const port = 5000;
 
 //middleware
 const songs = require('./sampleData/songs')
 const artists = require('./sampleData/artists')
-var bodyParser = require('body-parser')
+/* var bodyParser = require('body-parser')
 
 app.use(bodyParser.urlEncodeded({extended: true}))
-
+ */
 
 app.get('/', (req,res)=>{
-    res.send('Hello')
+    res.send('Welcome to the music artist api')
 })
 
+//Create new artist
 
+//Prints all artists
+app.get('/api/artists', (req,res)=>{
+    res.json(artists)
+})
+
+//Prints all songs
+app.get('/api/songs', (req,res)=>{
+    res.json(songs)
+})
+
+// Prints artist info by id
+app.get('/api/artists/:id', (req,res)=> {
+    const songInfo = artists.find((a) => a.id == req.params.id)
+    if(songInfo) res.json(songInfo)
+    else next()
+})
+
+//error handling
 app.use((req,res)=> {
     res.status(404)
-    res.json({error: `Resource not found`})
+    res.json({error: `Information not found`})
 })
 
 //port listen
--- sampleData/artists.js
+++ sampleData/artists.js
@@ -27,3 +27,5 @@ const artists = [
 
 
 ]
+
+module.exports = artists;
