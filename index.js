6dddac0a8ac9220640cab359e1501bcd556414c9
ChiomaE <chioma.eriken@gmail.com>
2024-03-14 12:33:08 -0500

    setup

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const app = express();
+const port = 3000;
+
+//middleware
+
+
+
+
+
+
+
+//port listen
+app.listen(port,()=>{
+    console.log(`This server is listening on port ${port}`)
+})
