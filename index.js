d9046b6103328a72656178a80ba963023adb2a30
ChiomaE <chioma.eriken@gmail.com>
2024-03-15 08:58:44 -0500

    body parser installed

--- index.js
+++ index.js
@@ -3,18 +3,40 @@ const app = express();
 const port = 5000;
 
 //middleware
+var bodyParser = require('body-parser')
 const songs = require('./sampleData/songs')
 const artists = require('./sampleData/artists')
-/* var bodyParser = require('body-parser')
 
-app.use(bodyParser.urlEncodeded({extended: true}))
- */
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json({extended:true}))
 
 app.get('/', (req,res)=>{
     res.send('Welcome to the music artist api')
 })
 
 //Create new artist
+app.post('/api/artists', (req,res) =>{
+    if(req.body.name && req.body.genre){
+        if(artists.find((a) => a.name == req.body.name)){
+            res.json({error:'This artist already exists'})
+        }
+
+        const artist = {
+            id: artists[artists.length-1].id+1,
+            name: req.body.name,
+            genre: req.body.genre
+        }
+
+        artists.push(newArtist)
+        res.json(artists[artists.length-1])
+    } else {
+        res.json({error: 'Insufficient Data'})
+    }
+
+
+})
+
+//Delete Artists
 
 //Prints all artists
 app.get('/api/artists', (req,res)=>{
